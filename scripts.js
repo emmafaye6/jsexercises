7118aff67e502e8e03409389f7798d817d348adb
Emma <111111773+emmafaye6@users.noreply.github.com>
2023-02-16 15:03:29 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,51 +1,75 @@
 // UC FIRST
 
-// initializing my hello world variable
-const helloWorld = "hello world";
+function ucFirst() {
+  // initializing my hello world variable
+  const helloWorld = "hello world";
 
-// using the charAt function to retrieve a character at a specific position
-// here, the first letter of the string
-const firstLetter = helloWorld.charAt(0);
+  // using the charAt function to retrieve a character at a specific position
+  // here, the first letter of the string
+  const firstLetter = helloWorld.charAt(0);
 
-// i capitalize the aforementioned first letter thanks to the toUpperCase method
-const firstLetterCapitalize = firstLetter.toUpperCase();
+  // i capitalize the aforementioned first letter thanks to the toUpperCase method
+  const firstLetterCapitalize = firstLetter.toUpperCase();
 
-// i use the slice method to separate the first letter from the rest of the string
-const lowercaseLetters = helloWorld.slice(1);
+  // i use the slice method to separate the first letter from the rest of the string
+  const lowercaseLetters = helloWorld.slice(1);
 
-// i concatenate the capitalized first letter and the rest of the string
-const capitalizedHello = firstLetterCapitalize + lowercaseLetters;
+  // i concatenate the capitalized first letter and the rest of the string
+  return firstLetterCapitalize + lowercaseLetters;
+}
 
-console.log(capitalizedHello);
+console.log(ucFirst());
 
 // CAPITALIZE
 
-// initializing my other hello world variable
-const capitalize = "hello world";
+function capitals() {
+  // initializing my other hello world variable
+  const capitalize = "hello world";
 
-// i split my hello world string into an array of words with the .split method
-const splitString = capitalize.split(" ");
+  // i split my hello world string into an array of words with the .split method
+  const splitString = capitalize.split(" ");
 
-// i create a for loop to loop over the array i just created. i initialize i as 0
-// as long as i is smaller than the length of the array, the loop continues to increment
-for (let i = 0; i < splitString.length; i++) {
-  // i pick the first letter of each word from the array and make it uppercase
-  // then add the capitalized letter to the rest of the string
-  splitString[i] = splitString[i][0].toUpperCase() + splitString[i].substr(1);
+  // i create a for loop to loop over the array i just created. i initialize i as 0
+  // as long as i is smaller than the length of the array, the loop continues to increment
+  for (let i = 0; i < splitString.length; i++) {
+    // i pick the first letter of each word from the array and make it uppercase
+    // then add the capitalized letter to the rest of the string
+    splitString[i] = splitString[i][0].toUpperCase() + splitString[i].substr(1);
+  }
+  // i turn the array back into a string of words with the .join method
+  return splitString.join(" ");
 }
-// i turn the array back into a string of words with the .join method
-console.log(splitString.join(" "));
+console.log(capitals());
 
 // REVERSE WORD
 
-// initializing hello world variable
-const word = "Hello World";
+function reverseWord() {
+  // initializing hello world variable
+  const word = "Hello World";
 
-// splitting the string into an array with split method
-const wordSplit = word.split(" ");
+  // splitting the string into an array with split method
+  const wordSplit = word.split(" ");
 
-// i use the reverse method to swap the string's two words
-const wordReverse = wordSplit.reverse();
+  // i use the reverse method to swap the string's two words
+  const wordReverse = wordSplit.reverse();
 
-// console logging my wordReverse variable while turning it back into a string
-console.log(wordReverse.join(" "));
+  // console logging my wordReverse variable while turning it back into a string
+  return wordReverse.join(" ");
+}
+
+console.log(reverseWord());
+
+// FUNCTION REPLACE VOWELS
+const quote = "postgresql";
+
+function replaceVowels() {
+  return quote
+    .replaceAll("a", "2")
+    .replaceAll("e", "z")
+    .replaceAll("i", "9")
+    .replaceAll("o", "-")
+    .replaceAll("u", "0")
+    .replaceAll("y", "6");
+}
+
+console.log(replaceVowels());
